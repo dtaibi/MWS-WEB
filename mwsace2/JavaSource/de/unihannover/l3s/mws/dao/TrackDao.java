59dcf90f102a5c3f5ed49fbea2739e622574e4e7
Davide Taibi <davide.taibi@itd.cnr.it>
2015-11-30 15:21:54 +0100

    Added Track files

--- mwsace2/JavaSource/de/unihannover/l3s/mws/dao/TrackDao.java
+++ mwsace2/JavaSource/de/unihannover/l3s/mws/dao/TrackDao.java
@@ -1,10 +1,16 @@
 package de.unihannover.l3s.mws.dao;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import de.unihannover.l3s.mws.model.SiteSet;
 import de.unihannover.l3s.mws.model.Track;
+import de.unihannover.l3s.mws.model.Utente;
 import de.unihannover.l3s.mws.util.HibernateUtil;
  
 public class TrackDao {
@@ -25,6 +31,24 @@ public class TrackDao {
             session.flush();
             session.close();
         }
+    }
+    
+    public List<Track> getAllSiteSetByUser(Utente user) {
+        List<Track> tracks = new ArrayList<Track>();
+        
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        try {
+        	String queryString = "from Track where utente = :user";
+            Query query = session.createQuery(queryString);
+            query.setLong("user", user.getId());
+            tracks = query.list();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        } finally {
+            session.flush();
+            session.close();
+        }
+        return tracks;
     }
  
    
