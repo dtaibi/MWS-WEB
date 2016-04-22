587790b6cb8b5daff00695d98727d71676f29cf1
Davide Taibi <davide.taibi@itd.cnr.it>
2016-04-22 17:40:56 +0200

    TrackDao

--- mwsace2/JavaSource/de/unihannover/l3s/mws/dao/TrackDao.java
+++ mwsace2/JavaSource/de/unihannover/l3s/mws/dao/TrackDao.java
@@ -8,7 +8,9 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import de.unihannover.l3s.mws.bean.TrackBean;
 import de.unihannover.l3s.mws.model.SiteSet;
+import de.unihannover.l3s.mws.model.Storyboard;
 import de.unihannover.l3s.mws.model.Track;
 import de.unihannover.l3s.mws.model.Utente;
 import de.unihannover.l3s.mws.util.HibernateUtil;
@@ -33,6 +35,90 @@ public class TrackDao {
         }
     }
     
+    //Anousheh 1
+    public List<Track> getAllTrackBeans() {
+		List<Track> list;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+        try {
+        	session.beginTransaction();
+        	
+            String queryString = "from Track t";
+            Query query = session.createQuery(queryString);
+            
+            list = query.list();
+            
+            return list;
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        } finally {
+            session.flush();
+            session.close();
+        }
+		return null;
+	}
+    
+    //Anousheh 2
+   public List<Track> getTrackByOperation(String operation) {
+	   List<Track> list;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        
+        try {
+        	session.beginTransaction();
+//        	String queryString = "from Track  t where t.operation in (select id from Operation o where o.role = :ro)";
+        	String queryString = "from Track t where operation = :operation";
+            Query query = session.createQuery(queryString);
+            query.setString("operation", operation);
+            list = query.list();
+            return list;
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        } finally {
+            session.flush();
+            session.close();
+        }
+        return null;
+    }
+
+   public List<Track> getTrackByUser(String utente) {
+	   List<Track> list;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        
+        try {
+        	session.beginTransaction();
+//        	String queryString = "from Track  t where t.operation in (select id from Operation o where o.role = :ro)";
+        	String queryString = "from Track t where utente = :utente";
+            Query query = session.createQuery(queryString);
+            query.setString("utente", utente);
+            list = query.list();
+            return list;
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        } finally {
+            session.flush();
+            session.close();
+        }
+        return null;
+    }
+  
+   public List<Utente> getAllUsersInTrack() {
+		List<Utente> list;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+       try {
+    	   session.beginTransaction();
+       	
+    	   String queryString = "from Utente ";
+           Query query = session.createQuery(queryString);
+           list = query.list();
+           return list;
+       } catch (RuntimeException e) {
+           e.printStackTrace();
+       } finally {
+           session.flush();
+           session.close();
+       }
+		return null;
+	}
+    
     public List<Track> getAllSiteSetByUser(Utente user) {
         List<Track> tracks = new ArrayList<Track>();
         
@@ -50,6 +136,28 @@ public class TrackDao {
         }
         return tracks;
     }
- 
+
+	public List<Track> getTrackByOperationAndUser(String operation,
+			String userselected) {
+		List<Track> list;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        
+        try {
+        	session.beginTransaction();
+        	String queryString = "from Track t where utente = :utente and operation = :operation";
+            Query query = session.createQuery(queryString);
+            query.setString("utente", userselected);
+            query.setString("operation", operation);
+            list = query.list();
+            return list;
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        } finally {
+            session.flush();
+            session.close();
+        }
+        return null;
+	}
+  	
    
 }
